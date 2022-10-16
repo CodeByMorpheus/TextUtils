df832117b0eca3a8ada72810773c3f11c3a7a48c
CodeByMorpheus <illogicalvibe@gmail.com>
2022-10-16 08:49:36 +0500

    Updates

---
+++ precache-manifest.809a3e796eb40e1c1de5ce32419b8d5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "994c05eaa2ee67f6504d",
+    "url": "/TextUtils/static/css/main.36db6c86.chunk.css"
+  },
+  {
+    "revision": "994c05eaa2ee67f6504d",
+    "url": "/TextUtils/static/js/main.994c05ea.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/TextUtils/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "63d048d7aecf146cced0",
+    "url": "/TextUtils/static/js/2.63d048d7.chunk.js"
+  },
+  {
+    "revision": "423d32646782b88757ba",
+    "url": "/TextUtils/static/js/runtime~main.423d3264.js"
+  },
+  {
+    "revision": "ed7539b541d8b52c6453dd60629539e1",
+    "url": "/TextUtils/index.html"
+  }
+];
